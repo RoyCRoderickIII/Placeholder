3cf3cd479bc6289ea5870f4f67128e16a9a26db3
CynicalOne <roycr3@gmail.com>
2024-06-21 09:41:46 -0400

    Creation of Script

---
+++ Start.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Setting up Needed Librarys
+import pip
+import subprocess
+import sys
+import json
+import time
+import requests
+import socket
+import os
+
+
+
+#C hecking/Install of necessary Files and or librarys
+# Check for Python3
+# Check for ADB
+# Import related librarys via Pip
+# Download of Miners Via Github
+# Compiling of Miners to maximize Cpu Architecture/Optimizations i.e neon avx2 etc
+ #Some sort of debug to make sure everything is installed and operational. 
+
+
+
+
+
